fce08d80b82ab633a44cfa21d684fefb5a155946
Finn Bacall <finn.bacall@cs.man.ac.uk>
2013-08-14 15:19:43 +0100

    Stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bootstrapp::Application.config.secret_token = '91913b60c02335ba69632679b05c4f18619fb2a4be308b8e7bd882e5f65461b345ce64be298f8811cab4913c5aaa176dadef12a60b3e9e26935f2a9336cd7126'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Bootstrapp::Application.config.session_store :cookie_store, key: '_bootstrapp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Bootstrapp::Application.config.session_store :active_record_store
